ce131ec554b3c9155284872db94bf3b58fe969da
artem <artemlisun0@gmail.com>
2024-04-13 12:25:56 -0500

    Initial commit

---
+++ src/main/java/org/nakedprogrammer/olympiad/OlympiadApplication.java
@@ -0,0 +1,13 @@
+package org.nakedprogrammer.olympiad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympiadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlympiadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nakedprogrammer/olympiad/OlympiadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nakedprogrammer.olympiad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympiadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
